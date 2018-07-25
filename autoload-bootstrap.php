87a2d2bfff2cee58b7d229afb311a6d513fd2cad
jrfnl <jrfnl@users.noreply.github.com>
2018-07-25 08:01:47 +0200

    Use PHPCompatibilityWP
    PHPCompatibility has released a new version `8.2.0` and in conjunction with that, has:
    - moved the repository to its own organisation
    - released a PHPCompatibilityWP ruleset which contains all the necessary excludes for backfills/polyfills provided by WP.
    
    This commit updates the project dependencies and rulesets to make use of this new `PHPCompatibilityWP` ruleset.

--- autoload-bootstrap.php
+++ autoload-bootstrap.php
@@ -7,8 +7,8 @@
  * @author   Juliette Reinders Folmer <qa_wp_projects_nospam@adviesenzo.nl>
  */
 
-if ( file_exists( __DIR__ . '/vendor/wimg/php-compatibility/PHPCSAliases.php' ) ) {
-	require_once __DIR__ . '/vendor/wimg/php-compatibility/PHPCSAliases.php';
+if ( file_exists( __DIR__ . '/vendor/phpcompatibility/php-compatibility/PHPCSAliases.php' ) ) {
+	require_once __DIR__ . '/vendor/phpcompatibility/php-compatibility/PHPCSAliases.php';
 }
 
 if ( file_exists( __DIR__ . '/vendor/wp-coding-standards/wpcs/WordPress/PHPCSAliases.php' ) ) {
